738fd2a518c5691d4ef3c6f3a5bd5af718d3fbfb
Pranjal-Sutar <143695657+Pranjal-Sutar@users.noreply.github.com>
2026-03-27 16:30:02 +0530

    Refactor Google Sheets integration to use env variable

--- sheets.py
+++ sheets.py
@@ -1,3 +1,5 @@
+import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -6,9 +8,12 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
+creds_dict = json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"))
+
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 
 client = gspread.authorize(creds)
 
-sheet = client.open("Your Sheet Name").sheet1
-data = sheet.get_all_records()
+def get_steps_raw():
+    sheet = client.open("YOUR_SHEET_NAME").sheet1
+    return sheet.get_all_records()
