a5de386bbed66dde77e436f75dcbd840cbf86977
Pranjal-Sutar <143695657+Pranjal-Sutar@users.noreply.github.com>
2026-04-15 12:28:16 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,35 +1,29 @@
-import os
-import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-creds_dict = json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"))
-
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-
-client = gspread.authorize(creds)
-
 def get_steps_raw(sheet_name):
-    scope = [...]
-    
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(
-        creds_dict, scope
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "streamlit_account.json",
+        scope
     )
 
     client = gspread.authorize(creds)
-
     sheet = client.open(sheet_name).sheet1
 
-    data = sheet.get_all_values()
+    rows = sheet.get_all_values()
 
-    steps = []
-    for row in data[3:]:  # skip first 3 rows
-        if row[2]:
-            steps.append({"description": row[2]})
+    tasks = []
+    for r in rows[1:]:  # skip header
+        if len(r) < 3:
+            continue
 
-    return steps
+        tasks.append({
+            "description": r[2]
+        })
+
+    return tasks
