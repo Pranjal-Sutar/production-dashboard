39f6e66ac2b4aca3f67ccbb1e180054c7368aac6
Pranjal-Sutar <143695657+Pranjal-Sutar@users.noreply.github.com>
2026-06-03 11:48:21 +0530

    Log Supabase URL and key existence
    Added print statements to log Supabase URL and key existence.

--- supabase_client.py
+++ supabase_client.py
@@ -7,4 +7,7 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
+# 👇 ADD THESE 2 LINES
+print("SUPABASE_URL =", repr(url))
+print("SUPABASE_KEY exists =", bool(key))
 supabase = create_client(url, key)
